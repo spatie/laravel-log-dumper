362e31da2cbb8e00af9e4e8c0d667634abb8e963
freek <freek@spatie.be>
2020-04-03 00:02:37 +0200

    wip

--- src/helpers.php
+++ src/helpers.php
@@ -3,7 +3,8 @@
 use Spatie\LogDumper\LogDumper;
 
 if (! function_exists('ld')) {
-    function ld(...$arguments) {
+    function ld(...$arguments)
+    {
         (new LogDumper())->log(...$arguments);
     }
 }
